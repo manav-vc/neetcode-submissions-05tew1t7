bc2359a9edee87957fbc89bc4350f1f212f7f360
Manav Chaudhary <120240843+manav-vc@users.noreply.github.com>
2026-04-05 14:06:56 -0400

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    public Map<Character, Integer> buildFreqMap(String s){
+            Map<Character, Integer> freq = new HashMap<>();
+            for(int i =0 ; i < s.length(); i++){
+                char c = s.charAt(i);
+                freq.put(c, freq.getOrDefault(c, 0) +1);
+            }
+
+            return freq;
+    }
+
+    public boolean isAnagram(String s, String t) {
+
+
+    Map<Character, Integer> freqMapS = buildFreqMap(s);
+    Map<Character, Integer> freqMapT = buildFreqMap(t);
+        
+    return freqMapS.equals(freqMapT);
+
+    }
+}
